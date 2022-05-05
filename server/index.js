8e81748c8a71e515e6dbdcd663d83bc0f60f415e
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2022-05-05 14:39:21 -0400

    Changes from yesterday

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 //Mongo DB
 const CONNECTION_URL = 'mongodb+srv://wesborland1234:ChipSpanPatriot1989@cluster0.v9ntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
