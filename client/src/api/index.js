19395800cd1e9a18fb21db0a9cf1f7a3231e17a6
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2022-05-08 23:55:41 -0400

    working on adding a post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,4 @@ const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 
-const addPost = (post) => axios.post(url, post);
+export const createPost = (newPost) => axios.post(url, newPost);
