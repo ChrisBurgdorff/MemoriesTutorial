33a9bd06591b5dfeda383a74f6d8bfc1e74e215c
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2022-05-15 02:06:30 -0400

    working on refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,25 @@
-import React, { useState, useEffect} from "react";
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Container } from '@material-ui/core';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import useStyles from './styles'
-
+import Navbar from "./components/Navbar/Navbar";
+import Home from  './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
   
-  const [currentId, setCurrentId] = useState(null);
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
 
   return(
-    <Container maxwidth="lg">
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxwidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
